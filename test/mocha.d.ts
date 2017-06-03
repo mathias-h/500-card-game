c278f522ef88812d68068444c52e7ce5ca20ec8f
Mathias Hansen <mathiasosthan@gmail.com>
2017-06-03 12:55:11 +0200

    added tests

--- test/mocha.d.ts
+++ test/mocha.d.ts
@@ -1,3 +1,3 @@
 declare function describe(name: string, func: Function): void
-declare function it(should: string, func: Function): void
+declare function it(should: string, func: (next: Function) => void): void
 declare function beforeEach(func: Function): void
